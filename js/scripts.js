4b2732690d47ccce0972795701f3d95208ff9e48
Chris Finney and Nathaniel Meyer <nathaniel@>
2017-02-21 13:35:06 -0800

    Separate business logic and UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
-  $("form#formOne").submit(function(event) {
-    var inputSentence = $("input#sentenceInput").val();
-      for (i =0; i < inputSentence.length; i++) {
-        if (inputSentence[i].match("[aeiouAEIOU]")) {
-          inputSentence = inputSentence.substring(0, i) + "-" + inputSentence.substring(i+1);
-      }
+
+  function vowelMask(string) {
+    for (i =0; i < string.length; i++) {
+      if (string[i] === "a" || string[i] === "e" || string[i] === "i" || string[i] === "o" || string[i] === "u" || string[i] === "A" || string[i] === "E" || string[i] === "I" || string[i] === "O" || string[i] === "U")
+      {string = string.substring(0, i) + "-" + string.substring(i+1);}
     }
+    return string;
+  }
+
+  $("form#formOne").submit(function(event) {
+    $("#sentenceGuess").text("");
+    var inputSentence = $("input#sentenceInput").val();
+    inputSentence = vowelMask(inputSentence);
     $("#outputSentence").show();
     $("#sentenceGuess").append(inputSentence);
     event.preventDefault();
